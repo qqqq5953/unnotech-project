b46d904a197866a694506c905538e99502f5f097
andy.H <qqqq5953@gmail.com>
2023-05-14 13:59:27 +0800

    Book.vue 使用共用元件

---
+++ src/components/forms/TextArea.vue
@@ -0,0 +1,27 @@
+<script setup>
+const props = defineProps({
+  modelValue: String,
+  placeholder: String,
+  isDisabled: {
+    type: Boolean,
+    default: false,
+  },
+  errors: {
+    type: Array,
+    default: [],
+  },
+});
+defineEmits(["update:modelValue"]);
+</script>
+
+<template>
+  <textarea
+    class="px-4 py-2 bg-white"
+    cols="30"
+    rows="10"
+    :value="modelValue"
+    @input="$emit('update:modelValue', $event.target.value)"
+    :placeholder="placeholder"
+    :disabled="isDisabled"
+  ></textarea>
+</template>
--- src/views/Book.vue
+++ src/views/Book.vue
@@ -1,9 +1,12 @@
 <script setup>
 import bookApi from "@/api/modules/bookApi.js";
 import { computed, ref } from "vue";
-import BaseModal from "@/components/BaseModal.vue";
+import BaseModal from "@/components/global/BaseModal.vue";
+import BaseHeader from "@/components/global/BaseHeader.vue";
+import BaseContainer from "@/components/global/BaseContainer.vue";
 import InputText from "@/components/forms/InputText.vue";
 import { notEmpty } from "@/tools/validators.js";
+import { useRouter } from "vue-router";
 
 const books = ref([]);
 const state = ref({
@@ -23,10 +26,10 @@ const state = ref({
   }
 })();
 
-const isOpen = ref(false);
-function toggleModal(open) {
-  if (!open) form.value.reset();
-  isOpen.value = open;
+const isModalOpen = ref(false);
+function toggleModal(isOpen) {
+  if (!isOpen) form.value.reset();
+  isModalOpen.value = isOpen;
 }
 
 const form = ref({
@@ -108,29 +111,28 @@ async function addBook() {
     state.value.loading = false;
   }
 }
+
+const router = useRouter();
+function viewDetail(book) {
+  router.push({ name: "Detail", params: { bookId: book.id } });
+}
 </script>
 
 <template>
   <Teleport to="body">
-    <div v-show="isOpen">
-      <BaseModal>
-        <template #header>
-          <header class="relative bg-white p-3 text-xl">
-            <button
-              class="absolute left-3 top-1/2 -translate-y-1/2 font-bold"
-              @click="toggleModal(false)"
-            >
-              <i class="fa-solid fa-arrow-left fa-xl"></i>
-            </button>
-            <h2 class="text-center">新增書本</h2>
-          </header>
+    <div v-show="isModalOpen">
+      <BaseModal modalTitle="新增書本">
+        <template #left>
+          <button
+            class="absolute left-3 top-1/2 -translate-y-1/2 font-bold"
+            @click="toggleModal(false)"
+          >
+            <i class="fa-solid fa-arrow-left fa-xl"></i>
+          </button>
         </template>
 
         <template #body>
-          <form
-            class="flex flex-col gap-y-6 p-3 h-full text-xs"
-            @submit.prevent="addBook"
-          >
+          <form class="flex flex-col gap-y-6 p-3 h-full text-xs mt-auto">
             <InputText
               class="mt-auto"
               v-model="form.title.value"
@@ -157,7 +159,11 @@ async function addBook() {
               >
                 取消
               </button>
-              <button type="submit" class="rounded-full bg-blue-500 w-1/2 p-3">
+              <button
+                type="button"
+                class="rounded-full bg-blue-500 w-1/2 p-3"
+                @click="addBook"
+              >
                 新增
               </button>
             </div>
@@ -167,50 +173,51 @@ async function addBook() {
     </div>
   </Teleport>
 
-  <div class="bg-slate-200">
-    <header class="relative bg-white p-3 text-xl">
-      <h2 class="text-center">書本列表</h2>
+  <BaseContainer headerTitle="書本列表">
+    <template #header-right-btn>
       <button
         class="absolute right-3 top-1/2 -translate-y-1/2 font-bold text-blue-500"
         @click="toggleModal(true)"
       >
         <i class="fa-solid fa-plus fa-xl"></i>
       </button>
-    </header>
+    </template>
+    <template #main>
+      <div class="grid place-items-center" v-if="state.loading">loading...</div>
+      <div v-else-if="state.error">{{ state.error }}</div>
 
-    <div class="grid place-items-center" v-if="state.loading">loading...</div>
-    <div v-else-if="state.error">{{ state.error }}</div>
-
-    <main class="py-3 md:py-6 lg:py-12" v-else>
-      <ul class="flex flex-wrap gap-y-3 md:gap-y-6 lg:gap-y-12">
-        <li class="w-1/2 group" v-for="book in books" :key="book.id">
-          <div
-            class="flex flex-col items-center gap-y-4 rounded bg-white hover:shadow-lg hover:cursor-pointer hover:border h-full text-xs p-3 group-odd:ml-3 group-odd:mr-1.5 group-even:mr-3 group-even:ml-1.5 group-odd:md:ml-6 group-odd:md:mr-3 group-even:md:ml-3 group-even:md:mr-6 md:p-6 group-odd:lg:ml-12 group-odd:lg:mr-6 group-even:lg:mr-12 group-even:lg:ml-6"
-          >
-            <div class="h-36 md:h-60 lg:h-80" v-if="book.image">
-              <img
-                class="block w-full h-full object-contain"
-                :src="book.image"
-                :alt="book.title"
-              />
-            </div>
+      <div class="py-3 md:py-6 lg:py-12" v-else>
+        <ul class="flex flex-wrap gap-y-3 md:gap-y-6 lg:gap-y-12">
+          <li class="w-1/2 group" v-for="book in books" :key="book.id">
             <div
-              class="grid place-items-center bg-slate-100 p-2 w-2/3 max-w-[170px] h-36 md:h-60 lg:h-80 lg:max-w-[250px]"
-              v-else
+              class="flex flex-col items-center gap-y-4 rounded bg-white hover:shadow-lg hover:cursor-pointer hover:border h-full text-xs p-3 group-odd:ml-3 group-odd:mr-1.5 group-even:mr-3 group-even:ml-1.5 group-odd:md:ml-6 group-odd:md:mr-3 group-even:md:ml-3 group-even:md:mr-6 md:p-6 group-odd:lg:ml-12 group-odd:lg:mr-6 group-even:lg:mr-12 group-even:lg:ml-6"
+              @click="viewDetail(book)"
             >
-              image lost...
+              <div class="h-36 md:h-60 lg:h-80" v-if="book.image">
+                <img
+                  class="block w-full h-full object-contain"
+                  :src="book.image"
+                  :alt="book.title"
+                />
+              </div>
+              <div
+                class="grid place-items-center bg-slate-100 p-2 w-2/3 max-w-[170px] h-36 md:h-60 lg:h-80 lg:max-w-[250px]"
+                v-else
+              >
+                image lost...
+              </div>
+              <h2 class="font-medium break-words line-clamp-2">
+                {{ book.title }}
+              </h2>
+              <p
+                class="mt-auto text-slate-600 text-right w-1/2 truncate self-end"
+              >
+                - {{ book.author }}
+              </p>
             </div>
-            <h2 class="font-medium break-words line-clamp-2">
-              {{ book.title }}
-            </h2>
-            <p
-              class="mt-auto text-slate-600 text-right w-1/2 truncate self-end"
-            >
-              - {{ book.author }}
-            </p>
-          </div>
-        </li>
-      </ul>
-    </main>
-  </div>
+          </li>
+        </ul>
+      </div>
+    </template>
+  </BaseContainer>
 </template>
